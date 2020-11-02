91d9b4013b862dbdcfafff83c991d341d8c0fd84
totoro <madfong@outlook.com>
2020-11-02 13:38:03 +0800

    some standard syntax

--- python/hello.py
+++ python/hello.py
@@ -1,2 +1,26 @@
 msg = "hello python"
 print(msg)
+print(msg + ", pyhton")
+print(msg * 10)
+
+a, b = 1, 1.1
+print(a,b)
+
+2 ** 3   # power
+7 % 3    # modulo
+7 // 3   # divide
+
+c, d = True, False
+c and d
+c or d
+not c
+
+e = None
+
+if c:
+    print("It's true")
+else:
+    print("It's false")
+
+for i in range(10):
+    print("hello python")
